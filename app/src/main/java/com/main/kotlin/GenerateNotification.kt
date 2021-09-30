dcc8e221cf396860d8a1396037c469c6aeffdada
Arvaan <paras.arvaan@gmail.com>
2021-09-30 19:41:45 +0530

    new flow

--- app/src/main/java/com/main/kotlin/GenerateNotification.kt
+++ app/src/main/java/com/main/kotlin/GenerateNotification.kt
@@ -4,16 +4,19 @@ import android.app.*
 import android.content.Context
 import android.content.Intent
 import android.graphics.BitmapFactory
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.collections.ArrayList
 
 
 class GenerateNotification : AppCompatActivity(), View.OnClickListener {
@@ -28,11 +31,13 @@ class GenerateNotification : AppCompatActivity(), View.OnClickListener {
     lateinit var tvFirstTime: TextView
     lateinit var tvSecondTime: TextView
     lateinit var tvThirdTime: TextView
+    lateinit var btnSubmit: Button
     var howManyCreateNotification = 0
     var dateLong: Long = 0L
 
     var calendarStartDate = Calendar.getInstance()
     var calendarFirstTime = Calendar.getInstance()
+    var calendarEndDate = Calendar.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_generate_notification)
@@ -50,10 +55,13 @@ class GenerateNotification : AppCompatActivity(), View.OnClickListener {
         tvFirstTime = findViewById(R.id.tvFirstTime)
         tvSecondTime = findViewById(R.id.tvSecondTime)
         tvThirdTime = findViewById(R.id.tvThirdTime)
+        btnSubmit = findViewById(R.id.btnSubmit)
 
         btnSelectTime.setOnClickListener(this)
         btnSelectSecondTime.setOnClickListener(this)
         btnSelectThirdTime.setOnClickListener(this)
+        btnSubmit.setOnClickListener(this)
+
     }
 
 
@@ -79,6 +87,7 @@ class GenerateNotification : AppCompatActivity(), View.OnClickListener {
 
     private fun selectStartDate() {
 
+
         val Year = calendarStartDate.get(Calendar.YEAR)
         val month = calendarStartDate.get(Calendar.MONTH)
         val dayOfMonth = calendarStartDate.get(Calendar.DAY_OF_MONTH)
@@ -88,7 +97,8 @@ class GenerateNotification : AppCompatActivity(), View.OnClickListener {
                 calendarStartDate[Calendar.YEAR] = year
                 calendarStartDate[Calendar.MONTH] = month
                 calendarStartDate[Calendar.DAY_OF_MONTH] = day
-                tvShowAll.text = day.toString() + "/" + (month + 1) + "/" + year
+                updateLabel(calendarStartDate, tvShowAll)
+
             }, Year,
             month,
             dayOfMonth
@@ -98,19 +108,19 @@ class GenerateNotification : AppCompatActivity(), View.OnClickListener {
     }
 
     fun selectEndDate() {
-        val calendar = Calendar.getInstance()
-        val year = calendar.get(Calendar.YEAR)
-        val month = calendar.get(Calendar.MONTH)
-        val dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH)
+
+        val year = calendarEndDate.get(Calendar.YEAR)
+        val month = calendarEndDate.get(Calendar.MONTH)
+        val dayOfMonth = calendarEndDate.get(Calendar.DAY_OF_MONTH)
 
         val datePickerDialog = DatePickerDialog(
             this,
             { datePicker, year, month, day ->
-                calendar[Calendar.YEAR] = year
-                calendar[Calendar.MONTH] = month
-                calendar[Calendar.DAY_OF_MONTH] = dayOfMonth
+                calendarEndDate[Calendar.YEAR] = year
+                calendarEndDate[Calendar.MONTH] = month
+                calendarEndDate[Calendar.DAY_OF_MONTH] = day
+                updateLabel(calendarEndDate, tvEndDate)
 
-                tvEndDate.text = day.toString() + "-" + (month + 1) + "-" + year
             },
             year,
             month,
@@ -174,12 +184,12 @@ class GenerateNotification : AppCompatActivity(), View.OnClickListener {
             dateLong = setAlarmCalender(concateString).timeInMillis
 
             Log.i("paras", "createNotification: $dateLong        $concateString")
-        }else if (howManyCreateNotification==2){
+        } else if (howManyCreateNotification == 2) {
             val concateString = "${tvShowAll.text} ${tvSecondTime.text}:00"
 
             dateLong = setAlarmCalender(concateString).timeInMillis
             Log.i("paras", "createNotification: $dateLong        $concateString")
-        }else if (howManyCreateNotification==3){
+        } else if (howManyCreateNotification == 3) {
 
             val concateString = "${tvShowAll.text} ${tvThirdTime.text}:00"
 
@@ -190,6 +200,7 @@ class GenerateNotification : AppCompatActivity(), View.OnClickListener {
 
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onClick(v: View?) {
 
         if (v != null) {
@@ -209,9 +220,14 @@ class GenerateNotification : AppCompatActivity(), View.OnClickListener {
                     selectThirdTime()
                     createNotification()
                 }
+                R.id.btnSubmit -> {
+
+                    submit(tvShowAll.text.toString(), tvEndDate.text.toString())
+                    Log.i("paras", "onClick: clicksubmit")
+                }
 
             }
-            Log.i("paras", "onClick: $howManyCreateNotification")
+            Log.i("paras", "onClick:-> $howManyCreateNotification")
         }
 
     }
@@ -259,5 +275,45 @@ class GenerateNotification : AppCompatActivity(), View.OnClickListener {
         alarmManager[AlarmManager.RTC_WAKEUP, delay] = pendingIntent
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun submit(first: String, second: String) {
+        val listOfDate = getDatesBetween(first, second)
+
+        for (date in listOfDate) {
+            Log.i("paras", "submit:$date ")
+        }
+
+    }
+
+
+    fun getDatesBetween(dateString1: String, dateString2: String): List<String> {
+        val dates = ArrayList<String>()
+        val input = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+        var date1: Date? = null
+        var date2: Date? = null
+        try {
+            date1 = input.parse(dateString1)
+            date2 = input.parse(dateString2)
+        } catch (e: ParseException) {
+            e.printStackTrace()
+        }
+        val cal1 = Calendar.getInstance()
+        cal1.time = date1
+        val cal2 = Calendar.getInstance()
+        cal2.time = date2
+        while (!cal1.after(cal2)) {
+            val output = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+            dates.add(output.format(cal1.time))
+            cal1.add(Calendar.DATE, 1)
+        }
+        return dates
+    }
+
+    private fun updateLabel(calendar: Calendar, view: TextView) {
+        val myFormat = "yyyy-MM-dd" //In which you need put here
+        val sdf = SimpleDateFormat(myFormat, Locale.getDefault())
+        view?.text = sdf.format(calendar.time)
+
+    }
 
 }
